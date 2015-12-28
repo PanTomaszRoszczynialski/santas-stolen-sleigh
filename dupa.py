6ca6c43a8d37485066abc7b20aec98d92c5eddeb
Tomasz Roszczynialski <roszcz@gmail.com>
2015-12-28 22:59:00 +0100

    Minor readability improvements and PEP8 compliance

--- dupa.py
+++ dupa.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 18 12:43:24 2015
-
-@author: tomek
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from haversine import haversine
@@ -15,7 +8,7 @@ north_pole = (90,0)
 sleigh_mass = 10.0
 total_wrw=0
 
-def readData(ListFile):
+def read_data(ListFile):
     f=open(ListFile,'rb')
     reader = csv.reader(f)
     GiftList = list()
@@ -23,7 +16,7 @@ def readData(ListFile):
         GiftList.append(map(float,raw[1:]))
     return GiftList    
     
-def writeSolution(GiftList,Trips,file_name):
+def write_solution(GiftList, Trips, file_name):
     with open(file_name,'w') as f :
         wrt = csv.writer(f,delimiter=',')
         wrt.writerow(['GiftId','TripId'])
@@ -32,13 +25,14 @@ def writeSolution(GiftList,Trips,file_name):
                 wrt.writerow([str(g),str(tr)])
     
 
-def euclid_dist(x,y):
+def euclid_dist(x, y):
+    """ Unused """
     return math.sqrt((x[1]-y[1])**2+(x[2]-y[2])**2)
 
-def boltzmannProb(dif,T):
+def boltzmann_distribution(dif, T):
     return np.exp(-dif/T)
     
-def initializeTrips(GiftList):
+def init_trips(GiftList):
     Trips = []
     for it in range(len(GiftList)):
         Trips.append([it])
@@ -47,7 +41,7 @@ def initializeTrips(GiftList):
     return Trips
 
 
-def trip_wrw(trip,Trips,GiftList,distance=haversine):
+def trip_wrw(trip, Trips, GiftList, distance=haversine):
   #  print trip
     route = Trips[trip]
     if len(route)==0 :
@@ -63,25 +57,25 @@ def trip_wrw(trip,Trips,GiftList,distance=haversine):
     tot+=sleigh_mass*haversine(places[len(places)-1],north_pole)
     return tot
     
-def initalize_wrws(Trips,GiftList,distance):
+def initalize_wrws(Trips, GiftList, distance):
     wrws = []
     for it in range(len(Trips)):
         
         wrws.append(trip_wrw(it,Trips,GiftList,distance))
     return wrws
     
-def totalWRW(Trips,GiftList,distance=haversine):
+def total_WRW(Trips, GiftList, distance=haversine):
     s=0
     for it in range(len(Trips)):
         s+=trip_wrw(it, Trips, GiftList,distance)
     return s
     
         
-def mergeTrips(trip1,trip2,Trips):
+def merge_trips(trip1, trip2, Trips):
     Trips[trip1]+=Trips[trip2]
     Trips[trip2]=list()
         
-def updateAfterMerge(trip1,trip2,GiftList):
+def update_after_merge(trip1, trip2, GiftList):
     if trip1==trip2:
         raise Exception(0)
     s=len(Trips[trip1])
@@ -92,18 +86,18 @@ def updateAfterMerge(trip1,trip2,GiftList):
         s+=1
            
         
-def checkIfMerge(trip1,trip2,Trips,GiftList,distance=haversine):
+def check_if_merge(trip1, trip2, Trips, GiftList, distance=haversine):
     l=list([list(Trips[trip1]),list(Trips[trip2])])
     l_c = list(l)
     Ei = trip_wrw(0,l,GiftList,distance)+trip_wrw(1,l,GiftList,distance)
     #print l
-    mergeTrips(0,1,l)
+    merge_trips(0,1,l)
     #print 'Jestem w checku',l
     Ef = trip_wrw(0,l,GiftList,distance)
     
     return Ef-Ei
     
-def permuteGiftsInTrip(new_route,GiftList,trip,Trips):
+def permute_gifts_in_trip(new_route, GiftList, trip, Trips):
     route = Trips[trip]
     #if trip!=GiftList[new_route[0]][3]:
     #    raise Exception(0)
@@ -111,19 +105,21 @@ def permuteGiftsInTrip(new_route,GiftList,trip,Trips):
         raise Exception(0)
     route = new_route
 
-def updateAfterPermutation(new_route,GiftList,Trips):
+def update_after_permutation(new_route, GiftList, Trips):
+    """ Unused """
     pass
     
-def checkIfPermute(new_route,GiftList,trip,Trips):
+def check_if_permute(new_route, GiftList, trip, Trips):
+    """ Unused """
     l=[list(Trips[trip])]
     E_i = trip_wrw(trip,Trips,GiftList)
-    permuteGiftsInTrip(new_route,GiftList,0,l)
+    permute_gifts_in_trip(new_route,GiftList,0,l)
     E_f = trip_wrw(0,l,GiftList)
     
     return E_f-E_i
         
 
-def avarageDifference(GiftList,Trips,samples):
+def avarage_difference(GiftList, Trips, samples):
     s_plus=0
     s_minus=0
     c_minus=0
@@ -139,7 +135,7 @@ def avarageDifference(GiftList,Trips,samples):
         trip2 = GiftList[id2][3]
         if trip1==trip2:
             continue
-        dif = checkIfMerge(trip1,trip2,Trips,GiftList)
+        dif = check_if_merge(trip1,trip2,Trips,GiftList)
         if dif <= 0 :
             c_minus-=1
         else :
@@ -148,62 +144,65 @@ def avarageDifference(GiftList,Trips,samples):
     result = [c_minus,c_plus,s_plus/N]
     return result
 
-def optimize1(T_start,iterations,GiftList,Trips,prob=boltzmannProb):
-    t_plus = 0
-    t_minus=0
-    wrw = totalWRW(Trips,GiftList)
-    epsilon = (T_start+100.0)/float(iterations)
-    T=T_start+epsilon
-    N=len(GiftList)
+def optimize1(T_start, iterations,
+              GiftList, Trips,
+              prob=boltzmann_distribution):
+    t_plus  = 0
+    t_minus = 0
+    wrw = total_WRW(Trips, GiftList)
+    epsilon = (T_start + 100.0)/float(iterations)
+    T = T_start + epsilon
+    N = len(GiftList)
     for it in range(iterations):
-        T-=epsilon
-        if T<=0 :
+        T -= epsilon
+        if T <= 0 :
             return wrw
-        print 'Temperature:',T
+
+        # Print debug information every 1000 steps
+        if it%1000 is 0:
+            print 'Temperature:', T
+
         id1 = random.randrange(0,N)
         id2 = random.randrange(0,N)
         gift1 = GiftList[id1]
         gift2 = GiftList[id2]
-        trip1,trip2 = gift1[3],gift2[3]
-        if trip1==trip2:
+        trip1, trip2 = gift1[3], gift2[3]
+        if trip1 == trip2:
             continue
  
         #print 'Trips',trip1,trip2,'chosen.'
-        dif = checkIfMerge(trip1,trip2,Trips,GiftList)
+        dif = check_if_merge(trip1,trip2,Trips,GiftList)
         #print  'Their difference: ',dif
         if dif < 0 :
             if len(Trips[trip1])+len(Trips[trip2])>=0.0 :
                 #print 'negative, merging'
-                updateAfterMerge(trip1,trip2,GiftList)
-                mergeTrips(trip1,trip2,Trips)
-                wrw+=dif
-                t_minus+=dif
+                update_after_merge(trip1, trip2, GiftList)
+                merge_trips(trip1, trip2, Trips)
+                wrw += dif
+                t_minus += dif
                 
                 #print Trips
 
         else :
             r = random.random()
             #print 'Boltzmann:', prob(dif,T),'random number:', r
-            if prob(dif,T)>r:
-                if len(Trips[trip1])+len(Trips[trip2])>=0.0:
+            if prob(dif,T) > r:
+                if len(Trips[trip1]) + len(Trips[trip2]) >= 0.0:
                     #print 'merging'
-                    updateAfterMerge(trip1,trip2,GiftList)
-                    mergeTrips(trip1,trip2,Trips)
+                    update_after_merge(trip1,trip2,GiftList)
+                    merge_trips(trip1,trip2,Trips)
                     wrw+=dif
                     t_plus+=dif
                     #print Trip
     return wrw
 
+GiftList = read_data('data/gifts.csv')[1:10000]
+Trips = init_trips(GiftList)
+#r = avarage_difference(GiftList,Trips,10000)
 
+before = total_WRW(Trips,GiftList)
 
-
-GiftList = readData('data/gifts.csv')[1:10000]
-Trips = initializeTrips(GiftList)
-#r = avarageDifference(GiftList,Trips,10000)
-
-before = totalWRW(Trips,GiftList)
-
-opt=optimize1(15000.0,100000,GiftList,Trips)
+opt = optimize1(15000.0,100000,GiftList,Trips)
 print 'Initial WRW', before
 print 'After optimization', opt
 s=0
@@ -213,24 +212,4 @@ for it in Trips:
     if len(it)!=0:
         n+=1
 print 'Avarage trip length:',float(s)/float(n)
-writeSolution(GiftList,Trips,'dupa1')
-
-
-
-#print w 
-
-
-
-
-
-#writeSolution(GiftList,'dupa1')
-
-
-
-
-
-
-
-
-
-        
+write_solution(GiftList,Trips,'local_solution.csv')
