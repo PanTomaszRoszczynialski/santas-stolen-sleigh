36fa03603922992eb7b1db46780465231a15ed95
Tomasz Roszczynialski <roszcz@gmail.com>
2016-01-03 17:51:07 +0100

    Imporve trips/gifts management

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from haversine import haversine
+import random
 
 north_pole = (90, 0)
 weight_limit = 1000
@@ -24,7 +25,7 @@ def read_gifts(path = "data/gifts.csv"):
 
     return gifts
 
-# Global gifts container
+# Global gifts container, use it to unhash gift information wid id only
 _GIFTS = read_gifts()
 
 def gift_mass(gid):
@@ -60,6 +61,14 @@ def show_gifts_info(gifts):
     print 'Latitude span: [{0}, {1}]'.format(min_lat, max_lat)
     print 'Longitude span: [{0}, {1}]'.format(min_long, max_long)
 
+def trip_info(trip):
+    """ Get distances between succeeding gifts """
+    prev_stop = north_pole
+    for gift in trip.gifts:
+        dist = distance(prev_stop, gift_position(gift))
+        prev_stop = gift_position(gift)
+        print 'Le distance: ', dist
+
 def find_on_meridian(gifts, long, sigma = 0.0):
     """ Finds gifts with the same longitude with accuracy of sigma """
 
@@ -69,6 +78,46 @@ def find_on_meridian(gifts, long, sigma = 0.0):
     out = gifts.loc[(gifts[_TH] < up_limit) & (gifts[_TH] > down_limit)]
 
     return out
+
+def find_on_parallel(gifts, latd, sigma = 0.01):
+    """ Find gifts with the same latitude """
+    up_limit   = latd + sigma
+    down_limit = latd - sigma
+    out = gifts.loc[(gifts[_PH] < up_limit) & (gifts[_PH] > down_limit)]
+
+    return out
+
+def trip_annealing(trip):
+    T_start = 100
+    iterations = 100
+    eps = (T_start + 4) / float(iterations)
+    T = T_start + eps
+
+    print 'starting with: ', trip.wrw()
+    Ei = trip.wrw()
+
+    for it in range(iterations):
+        T -= eps
+
+        if T <= 0:
+            print 'finishing with: ', trip.wrw()
+            return trip
+
+
+        jt = random.randrange(0, trip.length())
+        trip.shift(jt)
+
+        Ef = trip.wrw()
+
+        dif = Ef - Ei
+        print 'dif:', dif, 'jt: ', jt
+        if dif > 0:
+            r = random.random()
+            if np.exp(-dif/T) < r:
+                # Reverse swap 
+                trip.shift(-jt)
+
+    return trip
 
 class Trip(object):
     """ Basic holder for santa trips """
@@ -123,6 +172,10 @@ class Trip(object):
 
         return dist
 
+    def length(self):
+        """ Number of gifts in this trip """
+        return len(self.gifts)
+
     def remove_gift(self, gid):
         """ Remove gift with GiftId, not local index in self.list """
         self.gifts.remove(gid)
@@ -137,60 +190,101 @@ class Trip(object):
         """ Switch with gift from outside this trip """
         pass
 
+    def shift(self, roll):
+        """ Circshift's lookalike """
+        self.gifts = self.gifts[roll:] + self.gifts[:roll]
+
     def castling(self, first, second):
         """ Changing order of gifts within a trip """
         self.gifts[first], self.gifts[second] =\
         self.gifts[second], self.gifts[first]
-        pass
 
-if __name__ == '__main__':
-    not_delivered = _GIFTS.copy()
+    def trip_efficiency(self):
+        """ Return mass to wrw ratio of this trip """
+        return self.full_weight() / self.wrw()
+
+def generate_vertical_trips(not_delivered, sigma):
+    """ yo """
+    # Prepare tracing message
+    debug_str = 'Gifts left on the pole: {0}'
+    debug_str += ' after {1} trips.'
 
     trips = []
+    effs  = []
+    wrw   = 0
     while len(not_delivered.index) is not 0:
-        debug_str = 'Gifts left on the pole: {0}'
-        debug_str += ' after {1} trips.'
+        # Trace
         if len(trips)%20 == 0:
             print debug_str.format(len(not_delivered.index),
                                    len(trips))
+
         # Take random gift's longitude
         # sample() gets random row, iloc[0] enables label indexing
         long = not_delivered.sample().iloc[0][_TH]
 
         # Find all gifts on that longitude (with deviation sigma)
-        sigma = 0.05
         close = find_on_meridian(not_delivered, long, sigma=sigma)
         close = close.sort_values(_PH, ascending=False)
 
         # Take as many gifts on that linear trip as possible
-        while sum(close.Weight) > weight_limit:
+        while sum(close.Weight) > weight_limit - sleigh_weight:
             # Drop last FIXME optimazie this case 
-            close = close.drop(close.index[len(close.index)-1])
+            close = close.drop(close.index[0])
 
         # Create a trip with selected presents
         t = Trip()
         t.create_trip(close)
         trips.append(t)
+        local_wrw = t.wrw()
+        effs.append(t.full_weight() / local_wrw)
+        wrw += local_wrw
 
-        # Mark those gifts as delivered
+        # Mark those gifts as delivered (remove them)
         not_delivered = not_delivered.drop(close.index)
 
-        if len(trips) > 1e4:
-            break
+    out = {'Trips' : trips}
+    out.update({'WRW' : wrw})
+    out.update({'Efficency' : effs})
 
-    total = 0
-    for trip in trips:
-        total += trip.wrw()
+    return out
 
-    print 'Total WRW = ', total
+if __name__ == '__main__':
+    not_delivered = _GIFTS.copy()
 
+    sigma = 0.12
+    first_level = generate_vertical_trips(not_delivered, sigma)
 
-    # # Read with additional trip_id column
-    # gifts = read_gifts(path)
-    #
-    # # Use gifts table as a reference
-    # # again use gifts as *vec
-    # trips = first_level(gifts)
-    #
-    # # Take copy of trips?
-    # second_level(gifts, trips)
+    trips = first_level['Trips']
+    efficiencies = first_level['Efficency']
+
+    mean = np.mean(efficiencies)
+    std  = np.std(efficiencies)
+
+    good_trips = []
+    bad_trips  = []
+    for it, eff in enumerate(efficiencies):
+        if eff > mean:
+            bad_trips.append(trips[it])
+        else:
+            good_trips.append(trips[it])
+
+    good_wrw = 0
+    for good in good_trips:
+        good_wrw += good.wrw()
+
+    bad_wrw = 0
+    for bad in bad_trips:
+        bad_wrw += bad.wrw()
+
+    print 'good: ', good_wrw, 'bad: ', bad_wrw
+
+    # Create pandas container with undelivered gifts
+    undelivered  = []
+    for trip in bad_trips:
+        for gift in trip.gifts:
+            undelivered.append(gift)
+    # Go pandas
+    not_delivered = _GIFTS.loc[undelivered]
+    second_level = generate_vertical_trips(not_delivered, 2)
+
+    print 'total: ', second_level['WRW'] + good_wrw
